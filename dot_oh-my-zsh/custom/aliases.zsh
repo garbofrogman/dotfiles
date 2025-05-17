df43b6bcbb4c94294d770dc456b8680ef2a4c321
outlandishflmngo <102494168+garbofrogman@users.noreply.github.com>
2025-05-17 14:37:30 -0400

    Some config files idk

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+alias ls='ls -hF --color=auto'
+alias la='ls -ahF --color=auto'
+alias lah='ls -lahF --color=auto'
+
+alias grep='grep --color=auto'
+alias grepi='grep -i --color=auto'
+
+alias hyprconf='nvim ~/.config/hypr/hyprland.conf'
+
+alias vim='nvim'
+
+alias fehh='feh -.' # feh zoom to fill
+
+alias ni='firejail --net=none'
